a1fabfef760170b4c7d2c5c930c448135bd3a987
Peter Viechter <zelberor@schnalzerbuam.de>
2022-05-11 22:57:04 +0200

    Make iOS compatible wasm build by default

--- .build/buildWasm.js
+++ .build/buildWasm.js
@@ -4,5 +4,7 @@ import { execAsync, cdMTTesting, cdWebGl, cdThreadrrExample } from "./utils.js";
   // Actual script:
   await execAsync(`${cdWebGl}; wasm-pack build --target web --dev`);
   await execAsync(`${cdMTTesting}; wasm-pack build --target web --dev`);
-  await execAsync(`${cdThreadrrExample}; wasm-pack build --target web --dev`);
+  await execAsync(
+    `export WASM_BINDGEN_THREADS_MAX_MEMORY=8192; ${cdThreadrrExample}; wasm-pack build --target web --dev`
+  );
 })();
